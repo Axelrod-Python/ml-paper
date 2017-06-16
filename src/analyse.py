2a1b59b9c2af615b21533d955c8c38a3ad1f69c9
Vince Knight <vincent.knight@gmail.com>
2017-06-16 15:34:58 +0100

    Output results data.
    Add output of:
    
    - Scores
    - Payoff matrix
    - Payoff stdev
    
    To compressed gz file.

--- src/analyse.py
+++ src/analyse.py
@@ -1,4 +1,5 @@
 import axelrod as axl
+import numpy as np
 
 def main(filename):
 
@@ -6,10 +7,35 @@ def main(filename):
 
     prefix = "Standard" if "standard" in filename else "Noisy"
     results = axl.ResultSetFromFile(filename=filename, progress_bar=False)
-    # plot = axl.Plot(results)
-    # plot.save_all_plots(prefix="assets/{}".format(prefix), progress_bar=False,
-                        # title_prefix=prefix, filetype="pdf")
+    plot = axl.Plot(results)
+    plot.save_all_plots(prefix="assets/{}".format(prefix), progress_bar=False,
+                        title_prefix=prefix, filetype="pdf")
     results.write_summary("assets/{}_summary.csv".format(prefix))
+
+    # Write the total scores per tournament for each player to "assets/".
+    # This data is a #repetitions (rows) by #players (columns) array with
+    # X_{ij} corresponding to the TOTAL score obtained by player j in repetition
+    # i of the tournament.
+    scores_per_tournament = np.array(results.scores).transpose()
+    np.savetxt(fname="assets/{}_scores.gz".format(prefix),
+               X=scores_per_tournament)
+
+    # Write the payoff of each player against every other player to "assets/".
+    # This data is a #players (rows) by #players (rows) array with X_{ij}
+    # corresponding to the mean score of player i against player j
+    payoff_matrix = np.array(results.payoff_matrix)
+    np.savetxt(fname="assets/{}_payoff_matrix.gz".format(prefix),
+               X=payoff_matrix, delimiter=",")
+
+    # Write the stdv payoff of each player against every other player to
+    # "assets/". This data is a #players (rows) by #players (rows) array with
+    # X_{ij} corresponding to the standard deviation of the score of player i
+    # against player j
+    payoff_stdev_matrix = np.array(results.payoff_stddevs)
+    np.savetxt(fname="assets/{}_payoff_stdev_matrix.gz".format(prefix),
+               X=payoff_matrix, delimiter=",")
+
+
 
 if __name__ == "__main__":
     import sys
