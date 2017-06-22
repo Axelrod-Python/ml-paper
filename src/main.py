9bfab627a60a20942057cf4fe02805e7f4e18e4e
Vince Knight <vincent.knight@gmail.com>
2017-06-22 12:22:36 +0100

    Write analysis back in to main.
    The plan is to run smaller tournaments and amalgamate the data.

--- src/main.py
+++ src/main.py
@@ -13,8 +13,8 @@ def main(players=players, processes=None, seed=1, turns=200, repetitions=10000,
     kind = "std" if noise == 0 else "noisy"
     prefix = "Standard" if noise == 0 else "Noisy ({})".format(noise)
     filename = "data/strategies_{}_{}_{}_interactions.csv".format(kind,
-                                                               repetitions,
-                                                               seed)
+                                                                  repetitions,
+                                                                  seed)
     # Deleting the file if it exists
     try:
         os.remove(filename)
@@ -26,12 +26,49 @@ def main(players=players, processes=None, seed=1, turns=200, repetitions=10000,
     print("Axelrod version", axl.__version__)
     print(len(players), "Players")
     print("Seed", seed)
+    print("Repetitions", repetitions)
+    print("Noise", noise)
 
     tournament = axl.Tournament(players, turns=turns, repetitions=repetitions,
                                 noise=noise)
 
-    tournament.play(filename=filename, processes=processes,
-                    build_results=False, progress_bar=False)
+    results = tournament.play(filename=filename, processes=processes,
+                              progress_bar=False)
+
+
+    # Write summary for each seed
+    results.write_summary("assets/{}_{}_summary.csv".format(prefix, seed))
+
+    # Write the total scores per tournament for each player to "assets/".
+    # This data is a #repetitions (rows) by #players (columns) array with
+    # X_{ij} corresponding to the TOTAL score obtained by player j in repetition
+    # i of the tournament.
+    scores_per_tournament = np.array(results.scores).transpose()
+    np.savetxt(fname="assets/{}_{}_scores.gz".format(prefix, seed),
+               X=scores_per_tournament, delimiter=",")
+
+    # Write the total wins per tournament for each player to "assets/".
+    # This data is a #repetitions (rows) by #players (columns) array with
+    # X_{ij} corresponding to the TOTAL wins obtained by player j in repetition
+    # i of the tournament.
+    wins_per_tournament = np.array(results.wins).transpose()
+    np.savetxt(fname="assets/{}_{}_wins.gz".format(prefix, seed),
+               X=wins_per_tournament, delimiter=",")
+
+    # Write the payoff of each player against every other player to "assets/".
+    # This data is a #players (rows) by #players (rows) array with X_{ij}
+    # corresponding to the mean score of player i against player j
+    payoff_matrix = np.array(results.payoff_matrix)
+    np.savetxt(fname="assets/{}_{}_payoff_matrix.gz".format(prefix, seed),
+               X=payoff_matrix, delimiter=",")
+
+    # Write the stdv payoff of each player against every other player to
+    # "assets/". This data is a #players (rows) by #players (rows) array with
+    # X_{ij} corresponding to the standard deviation of the score of player i
+    # against player j
+    payoff_stdev_matrix = np.array(results.payoff_stddevs)
+    np.savetxt(fname="assets/{}_{}_payoff_stdev_matrix.gz".format(prefix, seed),
+               X=payoff_stdev_matrix, delimiter=",")
 
 if __name__ == "__main__":
     import sys
